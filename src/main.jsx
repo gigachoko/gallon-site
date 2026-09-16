81db2976e94ec2686d43949424d4be51e232d7eb
gigachoko <gigachoko@gmail.com>
2026-09-17 00:32:11 +0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GallonSite from "./GallonSite.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GallonSite />
+  </React.StrictMode>
+);
